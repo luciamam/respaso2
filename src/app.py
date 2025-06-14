358f05842eb447d7f0d24e98dfdec60e8fa0628f
luciamam <lumanmba2023@gmail.com>
2025-06-14 22:06:16 +0200

    guradando en los usuarios en la basse de datos

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template,request,redirect,url_for
 from  flask_bootstrap import Bootstrap4
 from forms.forms import RegisterFrom
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash,check_password_hash
 
 import os
 
@@ -17,6 +19,9 @@ load_dotenv()
 app=Flask(__name__)
 Bootstrap4(app)
 app.config['SECRET_KEY']=os.getenv("SECRET_KEY")
+client = MongoClient("mongodb://localhost:27017/")
+db=client["RepasoJunio2025"]
+usuarios=db["usuarios"]
 
 
 
@@ -24,12 +29,26 @@ app.config['SECRET_KEY']=os.getenv("SECRET_KEY")
 def home():
     return "soy la ruta raiz"
 
-
-
 @app.route('/register')
 def mostrar_register():
     form=RegisterFrom()
     return render_template('Register.html',form=form)
+
+
+@app.route('/register',methods=['POST'])
+def registrarse():
+    datos=request.form
+    name=datos['name']
+    email=datos['email']
+    password=datos['password']
+    #vamos a registrar al usuario en la basse de datos 
+    dict_usuario={
+        "name":name,
+        "email":email,
+        "password":generate_password_hash(password)
+    }
+    documento_usuario=usuarios.insert_one(dict_usuario)
+    return redirect(url_for('perfil'))
 
 
 
@@ -38,6 +57,10 @@ def mostrar_login():
     return "soy la ruta login"
 
 
+
+@app.route('/perfil')
+def perfil():
+    return "bienvenido a tu perfil"
 
 
 @app.errorhandler(404)
